4698c85ae48fb37c28406a8b608c0338aca0e280
Shadman Shaikh <shadmanforreal45@gmail.com>
2025-08-31 21:16:40 +0400

    added 1899a

---
+++ 800/1899a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void solve(){
+	int n;
+	cin >> n;
+	if( n % 3 == 0 ) cout<< "Second"<<'\n'; else cout<<"First"<<'\n';
+}
+int main(){
+	int t; cin>>t;
+	while(t--) solve();
+}
